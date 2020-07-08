3d8d0fe2232102cb4798e1003d5df796bb8fc237
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2020-07-08 20:37:31 +0100

    closed database connection in tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,13 +1,12 @@
 const request = require("supertest");
-
 const app = require("../src/app");
 const { closeDatabase } = require("./fixtures/db");
+
 describe("app.js test", () => {
   afterAll(closeDatabase);
 
   test("should get home page of api", async () => {
     const response = await request(app).get("/").expect(200);
-
     expect(response.body).not.toBeNull();
   });
 });
